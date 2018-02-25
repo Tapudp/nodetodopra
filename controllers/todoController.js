276d947f106b855eab80916563db75223e995943
Tapudp <divyeshpar2496@gmail.com>
2018-02-25 23:19:16 +0530

    first commit with all the files but the /assets/styles.css and the ejs in the main todoController.js and todo.ejs is not working

---
+++ controllers/todoController.js
@@ -0,0 +1,17 @@
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+
+module.exports = function(app) {
+
+app.get('/todo', (req, res) => {
+    res.render('todo', {todos: data});
+});
+
+app.post('/todo', (req, res) => {
+
+});
+
+app.delete('/todo/:item', (req, res) => {
+
+});
+
+};
